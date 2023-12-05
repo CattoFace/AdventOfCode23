28a099cee28e45b992f7c840d1470d86bb478dcc
Barr Israel <barr520@gmail.com>
2023-12-05 16:57:23 +0200

    update bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,4 @@
-use aoc23::{day1a, day1b, day2a, day2b, day3a, day3b};
+use aoc23::{day1a, day1b, day2a, day2b, day3a, day3b, day4a, day4b, day5a, day5b};
 use criterion::{criterion_group, criterion_main, Criterion};
 fn criterion_benchmark(c: &mut Criterion) {
     // c.bench_function("day1a", |b| b.iter(day1a::solve_day));
@@ -7,8 +7,10 @@ fn criterion_benchmark(c: &mut Criterion) {
     // c.bench_function("day2b", |b| b.iter(day2b::solve_day));
     // c.bench_function("day3a", |b| b.iter(day3a::solve_day));
     // c.bench_function("day3b", |b| b.iter(day3b::solve_day));
-    c.bench_function("day4a", |b| b.iter(day3b::solve_day));
-    c.bench_function("day4b", |b| b.iter(day3b::solve_day));
+    // c.bench_function("day4a", |b| b.iter(day4a::solve_day));
+    // c.bench_function("day4b", |b| b.iter(day4b::solve_day));
+    c.bench_function("day5a", |b| b.iter(day5a::solve_day));
+    c.bench_function("day5b", |b| b.iter(day5b::solve_day));
 }
 
 criterion_group!(benches, criterion_benchmark);
