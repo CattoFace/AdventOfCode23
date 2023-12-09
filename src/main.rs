f8390030ca5e47adbd0cfc1abb9dc41e6e68a279
Barr Israel <barr520@gmail.com>
2023-12-10 01:45:56 +0200

    setup dhat

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![feature(float_next_up_down)]
 mod day9b;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
     // for _i in 0..10000 {
     // day4b::solve_day();
     // }
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
     println!("{}", day9b::solve_day());
 }
