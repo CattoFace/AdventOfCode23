b5b7bbe7db0102c7221a4de1c00e6863e867b158
CattoFace <barr520@gmail.com>
2023-12-12 17:57:42 +0200

    day 12 finish

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,11 @@ mod day12b;
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
 fn main() {
-    // for _i in 0..10000 {
-    // day4b::solve_day();
-    // }
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
-    // let mut ans = 0u64;
-    // for _ in 0..100 {
-    // ans = day12b::solve_day()
-    // }
+    let mut ans = 0u64;
+    for _ in 0..100 {
+        ans = day12b::solve_day()
+    }
     println!("{}", day12b::solve_day());
 }
