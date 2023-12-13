8987886f46605183314d92290d08543da7cc51b6
Barr Israel <barr520@gmail.com>
2023-12-13 03:15:28 +0200

    day 12 final

--- src/day12b.rs
+++ src/day12b.rs
@@ -7,52 +7,80 @@ pub fn solve_day() -> u64 {
     solve_file(read_to_string("inputs/day12.txt").unwrap())
 }
 fn count_options(
-    cache: &mut HashMap<(usize, usize, u8, u64, bool), u64>,
-    line: &mut [u8],
-    nums: &mut [u64],
-    active: bool,
+    cache: &mut HashMap<(u8, u8, u8), u64>,
+    mut line: &mut [u8],
+    mut nums: &mut [u8],
+    mut run: u8,
 ) -> u64 {
-    // let s = std::str::from_utf8(line).unwrap();
-    // dbg!(s, &nums, &active);
+    let og_l = line.len() as u8;
+    let og_n = nums.len() as u8;
+    let og_r = run;
     if line.is_empty() {
-        if nums.is_empty() || (nums.len() == 1 && nums[0] == 0) {
+        if nums.is_empty() || (nums.len() == 1 && nums[0] == run) {
             return 1;
         } else {
             return 0;
         }
     }
-    if let Some(ans) = cache.get(&(line.len(), nums.len(), line[0], nums[0], active)) {
-        return *ans;
-    }
-    let c = line[0];
-    let out = match (c, active, nums[0]) {
-        (b'.', true, 0) => count_options(
-            cache,
-            &mut line[1..],
-            if nums.len() > 1 { &mut nums[1..] } else { nums },
-            false,
-        ),
-        (b'.', true, _) => 0,
-        (b'.', false, _) => count_options(cache, &mut line[1..], nums, false),
-        (b'#', true, 0) => 0,
-        (b'#', false, 0) => 0,
-        (b'#', _, _) => {
-            nums[0] -= 1;
-            let ans = count_options(cache, &mut line[1..], nums, true);
-            nums[0] += 1;
-            ans
+    let mut extra = 0;
+    let mut out = loop {
+        // let s = std::str::from_utf8(line).unwrap();
+        // dbg!(s, &nums, &run);
+        if let Some(ans) = cache.get(&(line.len() as u8, nums.len() as u8, run)) {
+            return *ans;
         }
-        (b'?', _, _) => {
-            line[0] = b'.';
-            let mut ans = count_options(cache, line, nums, active);
-            line[0] = b'#';
-            ans += count_options(cache, line, nums, active);
-            line[0] = b'?';
-            ans
+        let c = line[0];
+        match c {
+            b'.' => match run {
+                0 => {}
+                _ => {
+                    if !nums.is_empty() && run == nums[0] {
+                        nums = &mut nums[1..];
+                        run = 0;
+                    } else {
+                        break 0;
+                    }
+                }
+            },
+            b'#' => {
+                if !nums.is_empty() && run <= nums[0] {
+                    run += 1;
+                } else {
+                    break 0;
+                }
+            }
+            b'?' => {
+                // calculate extra as .
+                extra += match run {
+                    0 => count_options(cache, &mut line[1..], nums, 0),
+                    _ => {
+                        if !nums.is_empty() && run == nums[0] {
+                            count_options(cache, &mut line[1..], &mut nums[1..], 0)
+                        } else {
+                            0
+                        }
+                    }
+                };
+                // continue as #
+                if !nums.is_empty() && run <= nums[0] {
+                    run += 1;
+                } else {
+                    break 0;
+                }
+            }
+            _ => unreachable!(),
+        };
+        line = &mut line[1..];
+        if line.is_empty() {
+            if nums.is_empty() || (nums.len() == 1 && nums[0] == run) {
+                break 1;
+            } else {
+                break 0;
+            }
         }
-        _ => unreachable!(),
     };
-    cache.insert((line.len(), nums.len(), line[0], nums[0], active), out);
+    out += extra;
+    cache.insert((og_l, og_n, og_r), out);
     out
 }
 fn solve_file(text: String) -> u64 {
@@ -79,13 +107,13 @@ fn solve_file(text: String) -> u64 {
             new_nums_str.push_str(nums_str);
             let mut nums = new_nums_str
                 .split(',')
-                .map(|s| s.parse::<u64>().unwrap())
+                .map(|s| s.parse::<u8>().unwrap())
                 .collect_vec();
             // let mut values_string = values_str.to_string();
             let values_arr = unsafe { new_values_str.as_bytes_mut() };
-            let mut cache: HashMap<(usize, usize, u8, u64, bool), u64> =
-                HashMap::with_capacity(new_nums_str.len() * values_arr.len());
-            count_options(&mut cache, values_arr, &mut nums, false)
+            let mut cache: HashMap<(u8, u8, u8), u64> =
+                HashMap::with_capacity(new_nums_str.len() * values_arr.len() / 3);
+            count_options(&mut cache, values_arr, &mut nums, 0)
         })
         .sum()
 }
