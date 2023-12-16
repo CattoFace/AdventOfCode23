0150b62a4729a7d0c707e9276ef43c4f3222d18f
Barr Israel <barr520@gmail.com>
2023-12-17 00:19:01 +0200

    day 16 final

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ static ALLOC: dhat::Alloc = dhat::Alloc;
 fn main() {
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
-    let mut ans = 0u32;
-    for _ in 0..100000 {
-        ans = day16a::solve_day()
-    }
-    dbg!(&ans);
-    // println!("{}", day16a::solve_day());
+    // let mut ans = 0u32;
+    // for _ in 0..100000 {
+    //     ans = day16a::solve_day()
+    // }
+    // dbg!(&ans);
+    println!("{}", day16a::solve_day());
 }
