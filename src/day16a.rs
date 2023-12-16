0e3879bd15d44e2299aeb0c4f4e513938986675c
Barr Israel <barr520@gmail.com>
2023-12-16 12:29:18 +0200

    day 16

---
+++ src/day16a.rs
@@ -0,0 +1,152 @@
+use std::fs::read_to_string;
+
+pub fn solve_day() -> u32 {
+    solve_file(read_to_string("inputs/day16.txt").unwrap())
+}
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn ray(
+    text: &mut [u8],
+    width: usize,
+    height: usize,
+    mut x: usize,
+    mut y: usize,
+    mut direction: Direction,
+) -> u32 {
+    use Direction::*;
+    let mut ans = 0u32;
+    let mut index = width * y + x;
+    // 8th bit marks energized
+    while text[index] != (128 | b'-') && text[index] != (128 | b'|') {
+        ans += (text[index] & 128 == 0) as u32;
+        text[index] |= 128;
+        match (text[index] & 127, &direction) {
+            (b'.' | b'|', Up) => {
+                if y == 0 {
+                    break;
+                };
+                y -= 1;
+            }
+            (b'.' | b'|', Down) => {
+                if y == height - 1 {
+                    break;
+                }
+                y += 1;
+            }
+            (b'.' | b'-', Left) => {
+                if x == 0 {
+                    break;
+                }
+                x -= 1;
+            }
+            (b'.' | b'-', Right) => {
+                if x == width - 2 {
+                    break;
+                }
+                x += 1;
+            }
+            (b'\\', Up) | (b'/', Down) => {
+                if x == 0 {
+                    break;
+                }
+                x -= 1;
+                direction = Left;
+            }
+            (b'\\', Down) | (b'/', Up) => {
+                if x == width - 2 {
+                    break;
+                }
+                x += 1;
+                direction = Right
+            }
+            (b'\\', Left) | (b'/', Right) => {
+                if y == 0 {
+                    break;
+                }
+                y -= 1;
+                direction = Up
+            }
+            (b'\\', Right) | (b'/', Left) => {
+                if y == height - 1 {
+                    break;
+                }
+                y += 1;
+                direction = Down
+            }
+            (b'-', Up | Down) => {
+                if x < width - 2 {
+                    ans += ray(text, width, height, x + 1, y, Right);
+                }
+                if x == 0 {
+                    break;
+                }
+                x -= 1;
+                direction = Left;
+            }
+            (b'|', Left | Right) => {
+                if y < height - 1 {
+                    ans += ray(text, width, height, x, y + 1, Down);
+                }
+                if y == 0 {
+                    break;
+                }
+                y -= 1;
+                direction = Up;
+            }
+            _ => unreachable!(
+                "Reached with {},{},{},{},{:?}",
+                x,
+                y,
+                (text[index] & 127) as char,
+                text[index] & 128,
+                direction
+            ),
+        }
+        index = width * y + x;
+        // print_grid(text, index);
+    }
+    ans
+}
+
+#[allow(dead_code)]
+fn print_grid(text: &[u8], index: usize) {
+    println!();
+    text.iter().enumerate().for_each(|(i, &c)| {
+        if i == index {
+            print!("O");
+        } else if c & 128 == 0 {
+            print!("{}", c as char);
+        } else {
+            print!("#");
+        }
+    })
+}
+fn solve_file(text: String) -> u32 {
+    let mut binding = text.into_bytes();
+    let text: &mut [u8] = binding.as_mut();
+    let width = text.iter().position(|&c| c == b'\n').unwrap() + 1;
+    let height = text.len() / width;
+    ray(text, width, height, 0, 0, Direction::Right)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn solve_test() {
+        assert_eq!(
+            solve_file(read_to_string("inputs/day16_test.txt").unwrap()),
+            46
+        );
+        assert_eq!(
+            solve_file(read_to_string("inputs/day16.txt").unwrap()),
+            7788
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(float_next_up_down)]
-mod day15b;
+mod day16b;
 
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
@@ -10,8 +10,8 @@ fn main() {
     let _profiler = dhat::Profiler::new_heap();
     // let mut ans = 0u32;
     // for _ in 0..100000 {
-    //     ans = day15b::solve_day()
+    //     ans = day16b::solve_day()
     // }
     // dbg!(&ans);
-    println!("{}", day15b::solve_day());
+    println!("{}", day16b::solve_day());
 }
