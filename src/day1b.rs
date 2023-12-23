2ea5634fb97b1fec6c124cdd998be05bea2fba62
Barr Israel <barr520@gmail.com>
2023-12-23 04:32:29 +0200

    redo day 1

--- src/day1b.rs
+++ src/day1b.rs
@@ -1,11 +1,11 @@
 use std::fs::read_to_string;
 
-pub fn solve_day() -> u32 {
+pub fn solve_day() -> u16 {
     solve_file(read_to_string("inputs/day1.txt").unwrap())
 }
-fn solve_file(text: String) -> u32 {
-    let map: [&[u8]; 10] = [
-        b"zero", b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
+fn solve_file(text: String) -> u16 {
+    let map: [&[u8]; 9] = [
+        b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
     ];
     let text = text.as_bytes();
     text.split(|&c| c == b'\n')
@@ -14,24 +14,24 @@ fn solve_file(text: String) -> u32 {
                 return 0;
             }
             // println!("{}", str::from_utf8(line).unwrap());
-            let first_num = loop {
+            let first_num = (loop {
                 if let Some(res) = (0..map.len())
-                    .position(|n| line[0] - b'0' == n as u8 || line.starts_with(map[n]))
+                    .position(|n| line[0] - b'1' == n as u8 || line.starts_with(map[n]))
                 {
                     break res;
                 }
                 line = &line[1..];
-            } as u32
+            } + 1) as u16
                 * 10;
-            let last_num = loop {
+            let last_num = (loop {
                 if let Some(res) = (0..map.len())
-                    .position(|n| line[line.len() - 1] - b'0' == n as u8 || line.ends_with(map[n]))
+                    .position(|n| line[line.len() - 1] - b'1' == n as u8 || line.ends_with(map[n]))
                 {
                     break res;
                 }
                 line = &line[..line.len() - 1];
-            };
-            first_num + last_num as u32
+            } + 1) as u16;
+            first_num + last_num
         })
         .sum()
 }
